d0db94620d9e16b546a35d667c6a82e6f39a3d6c
bharath <bharath4657@gmail.com>
2024-01-14 16:36:29 +0530

    asdsadkas

--- Ansible/roboshop.sh
+++ Ansible/roboshop.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+
 AMI=ami-01faf8de678b6d856
-SG_ID=sg-0b677afd1b217e8ef
-INSTANCES=("mongodb" "redis" "mysql" "cart" "catalogue" "payment" "shipping" "user" "rabbitmq" "dispatch" "web")
-ZONE_ID=Z07187862EB134EMJD6GR
+SG_ID= sg-0b677afd1b217e8ef #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z07187862EB134EMJD6GR # replace your zone ID
 DOMAIN_NAME="bpix.online"
 
 for i in "${INSTANCES[@]}"
 do
-if [ $i == "mongodb"] || [ $i == "mysql"] || [ $i == "shipping"]
-then 
-INSTANCE_TYPE="t3.small"
-else 
-INSTANCE_TYPE="t2.micro"
-fi
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
 
-IP_ADDRESS= $(aws ec2 run-instances --image-id ami-01faf8de678b6d856 --instance-type $INSTANCE_TYPE --security-group-ids sg-0b677afd1b217e8ef --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
+    #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change-batch
- 
- {
+    --change-batch '
+    {
         "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
         "Action"              : "UPSERT"
@@ -37,6 +38,3 @@ IP_ADDRESS= $(aws ec2 run-instances --image-id ami-01faf8de678b6d856 --instance-
     }
         '
 done
-
-
-
