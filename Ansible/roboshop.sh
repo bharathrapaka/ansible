10c2fb9ecab6cba2128bb860b60c96cd57210f2b
bharath <bharath4657@gmail.com>
2024-01-15 13:14:20 +0530

    Your commit message heraseasas

--- Ansible/roboshop.sh
+++ Ansible/roboshop.sh
@@ -18,7 +18,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0b677afd1b217e8ef --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
-    #create R53 record, make sure you delete existing record
+    
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
