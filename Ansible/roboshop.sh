1a0808e1e9813edca44636ed24577db018e69d45
bharath <bharath4657@gmail.com>
2024-01-15 13:15:32 +0530

    sadkjsakjd

--- Ansible/roboshop.sh
+++ Ansible/roboshop.sh
@@ -16,7 +16,7 @@ do
     fi
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0b677afd1b217e8ef --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "$i: $IP_ADDRESS"
+    echo "$i: $IP_ADDRESS" #sdasdsad
 
     
     aws route53 change-resource-record-sets \
